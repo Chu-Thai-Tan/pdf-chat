8f56ddda638d2adb22f86e3cd7e9fc3f89308220
Tan Chu <chuthaitan91098@gmail.com>
2024-03-31 11:22:35 +0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import {
+  ChangeEvent,
+  ChangeEventHandler,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+
+export default function Home() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [pdfFile, setPdfFile] = useState<any>(null);
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    let selectedFiles = e.target.files?.[0];
+    if (selectedFiles) {
+      let reader = new FileReader();
+      reader.readAsDataURL(selectedFiles);
+      reader.onload = (e) => {
+        setPdfFile(e.target?.result);
+      };
+    }
+  };
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <input
+        ref={inputRef}
+        type="file"
+        name="Add pdf file"
+        id=""
+        accept="application/pdf"
+        onChange={handleChange}
+      />
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+        {pdfFile && (
+          <div className="w-700 h-750">
+            <Viewer fileUrl={pdfFile} plugins={[defaultLayoutPluginInstance]} />
+          </div>
+        )}
+      </Worker>
+    </main>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      height: {
+        "750": "750px",
+      },
+      width: {
+        "500": "500px",
+        "600": "600px",
+        "700": "700px",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
